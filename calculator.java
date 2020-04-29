9f9ac75c953f99dac803120795112dae712feaf3
SobakaPirat <40334641+SobakaPirat@users.noreply.github.com>
2020-04-29 09:07:07 +0300

    input and parsing

--- calculator.java
+++ calculator.java
@@ -1,5 +1,21 @@
 import java.util.Scanner;
 
-public class Calculator {
-    
+public class calculator {
+    public static void main(String[] args) {
+        int num1;
+        int num2;
+        String operation;
+
+
+        Scanner input = new Scanner(System.in);//ввод
+        System.out.println("введите пример");
+
+        num1 = input.nextInt();
+        operation = input.next();
+        num2 = input.nextInt();
+
+        //System.out.println(num1 + " " + operation + " " + num2);
+        //проверка ввода
+    }
+
 }
