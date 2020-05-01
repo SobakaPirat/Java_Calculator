ed53a78b6c53959745436994648a7767019828b2
SobakaPirat <40334641+SobakaPirat@users.noreply.github.com>
2020-05-01 10:56:24 +0300

    complete Calculations, make arabics and romans array way bigger for multiply.

--- calculator.java
+++ calculator.java
@@ -49,8 +49,8 @@ public class calculator {
     }
     public static class ROMAN{
         static int roman_checker = 0; //если 0 - вывод арабских; 2 - римских; 1 - ошибка.
-        static String [] romans = {"I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"};
-        static String [] arabics = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"};
+        static String [] romans = {"I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX","XXI","XXII","XXIII","XXIV","XXV","XXVI","XXVII","XXVIII","XXIX","XXX","XXXI","XXXII","XXXIII","XXXIV","XXXV","XXXVI","XXXVII","XXXVIII","XXXIX","XL","XLI","XLII","XLIII","XLIV","XLV","XLVI","XLVII","XLVIII","XLIX","L","LI","LII","LIII","LIV","LV","LVI","LVII","LVIII","LIX","LX","LXI","LXII","LXIII","LXIV","LXV","LXVI","LXVII","LXVIII","LXIX","LXX","LXXI","LXXII","LXXIII","LXXIV","LXXV","LXXVI","LXXVII","LXXVIII","LXXIX","LXXX","LXXXI","LXXXII","LXXXIII","LXXXIV","LXXXV","LXXXVI","LXXXVII","LXXXVIII","LXXXIX","XC","XCI","XCII","XCIII","XCIII","XCIV","XCV","XCVI","XCVII","XCVIII","XCIX","C"};
+        static String [] arabics = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60","61","62","63","64","65","66","67","68","69","70","71","72","73","74","75","76","77","78","79","80","81","82","83","84","85","86","87","88","89","90","91","92","93","94","95","96","97","98","99","100"};
 
         public static int ToInt(String number){
             
@@ -72,7 +72,7 @@ public class calculator {
         public static String ToRoman(int answer){
             String str_answer = String.valueOf(answer);
             if (roman_checker == 2){
-                for (int i = 0; i < 20; i++){
+                for (int i = 0; i < 100; i++){
                     if (str_answer.equals(arabics[i])){
                         roman_checker++;
                         str_answer = romans[i];
@@ -117,13 +117,13 @@ public class calculator {
             return (num1_int + num2_int);
         }
         public static int minus(int num1_int,int num2_int){
-            return (num1_int + num2_int);
+            return (num1_int - num2_int);
         }
         public static int multiply(int num1_int,int num2_int){
-            return (num1_int + num2_int);
+            return (num1_int * num2_int);
         }
         public static int divide(int num1_int,int num2_int){
-            return (num1_int + num2_int);
+            return (num1_int / num2_int);
         }
 
     }
