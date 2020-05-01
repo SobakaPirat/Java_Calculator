02dd2640f216f783cc65309071b553c2c8ecacba
SobakaPirat <40334641+SobakaPirat@users.noreply.github.com>
2020-05-01 09:31:36 +0300

    add classes and checkers.

--- calculator.java
+++ calculator.java
@@ -1,68 +1,88 @@
 import java.util.Scanner;
 
+
 public class calculator {
     static Scanner scanner = new Scanner(System.in);
  
+    
     public static void main(String[] args) {
-        parse();
+            
+
+        PARSER.parse();
         //int result = calc(num1,num2,operation);
         //System.out.println("Результат операции: "+result);
     }
+    
 
-    public static void parse()
-    {
-        String num1;
-        String num2;
-        int num1_int;
-        int num2_int;
-        String operation;
+    public static class PARSER{
+        public static void parse()
+        {   
+
+            String num1;
+            String num2;
+            int num1_int;
+            int num2_int;
+            
+
+            String operation;
 
 
-        Scanner input = new Scanner(System.in, "cp866");//ввод
-        System.out.println("введите пример");
+            Scanner input = new Scanner(System.in);//ввод
+            System.out.println("vvedite primer");
 
-        num1 = input.next();
-        num1_int = ToInt(num1);
-        operation = input.next();
-        num2 = input.next();
-        num2_int = ToInt(num2);
+            num1 = input.next();
+            num1_int = ROMAN.ToInt(num1);
+            CHECKER.limit(num1_int);
+            operation = input.next();
+            num2 = input.next();
+            num2_int = ROMAN.ToInt(num2);
+            CHECKER.limit(num2_int);
+            CHECKER.roman();
 
-        System.out.println(num1_int + " " + operation + " " + num2_int);
-        //проверка ввода
+            System.out.println(num1_int + " " + operation + " " + num2_int);
+            //проверка ввода
 
-    }
-    public static int ToInt(String number){
-        number = number.toUpperCase();
-        int num_int = 0;
-        int roman_checker = 0; //если 0 - вывод арабских; 2 - римских; 1 - ошибка.
-        String [] romans = {"I","II","III","IV","V","VI","VII","VIII","IX","X"};
-        String [] arabics = {"1","2","3","4","5","6","7","8","9","10"};
-        for (int i = 0; i < 10; i++){
-            if (romans[i].equals(number)){
-            roman_checker++;
-            num_int = i + 1;
-            }    
         }
-        for (int i = 0; i < 10; i++){
-            if (arabics[i].equals(number)){
-            num_int = i + 1;
-            }    
-        }
-        return (num_int);
     }
+    public static class ROMAN{
+        static int roman_checker = 0; //если 0 - вывод арабских; 2 - римских; 1 - ошибка.
+        static String [] romans = {"I","II","III","IV","V","VI","VII","VIII","IX","X"};
+        static String [] arabics = {"1","2","3","4","5","6","7","8","9","10"};
 
-    public static int Сheck(String number){
-        boolean stop_checker = true;
-        int num_checker = 0;
-        while (stop_checker) {
-            num_checker = ToInt(number);
-            if (num_checker == 0){
-                System.out.println("Число вышло за границы. Попробуйте ещё раз.");
-            } else {
-                stop_checker = false;
+        public static int ToInt(String number){
+            
+            number = number.toUpperCase();
+            int num_int = 0;
+            for (int i = 0; i < 10; i++){
+                if (romans[i].equals(number)){
+                roman_checker++;
+                num_int = i + 1;
+                }    
+            }
+            for (int i = 0; i < 10; i++){
+                if (arabics[i].equals(number)){
+                num_int = i + 1;
+                }    
+            }
+            return (num_int);
+        }
+    }
+    
+    public static class CHECKER{
+
+        public static void limit(int check_number){
+            if (check_number == 0) {
+                System.out.println("chisla ne nahodyatsya v predelah 1-10; i-x.");
+                System.exit(0);
+            } 
+        }
+        public static void roman(){
+            if (ROMAN.roman_checker == 1){
+                System.out.println("chisla dolzhni bit' tol'ko arbskimi, libo rimskimi.");
+                System.exit(0);
             }
         }
-        return (num_checker);
+
     }
     
 
