dde0ef611c73de4fe9ab3d3eac6233333f90af0f
SobakaPirat <40334641+SobakaPirat@users.noreply.github.com>
2020-05-01 11:13:35 +0300

    translate and fix bugs

--- calculator.java
+++ calculator.java
@@ -21,27 +21,24 @@ public class calculator {
             int num1_int;
             int num2_int;
             int  answer;
-            
-
             String operation;
 
 
             Scanner input = new Scanner(System.in);//ввод
-            System.out.println("vvedite primer");
+            System.out.println("enter an example");
 
             num1 = input.next();
             num1_int = ROMAN.ToInt(num1);
             CHECKER.limit(num1_int);
+
             operation = input.next();
+
             num2 = input.next();
             num2_int = ROMAN.ToInt(num2);
             CHECKER.limit(num2_int);
+
             CHECKER.roman();
             
-            
-
-            //System.out.println(num1_int + " " + operation + " " + num2_int);
-            //проверка ввода
 
             answer = (CHECKER.opearation(num1_int, num2_int, operation));
             System.out.println(ROMAN.ToRoman(answer));
@@ -49,10 +46,10 @@ public class calculator {
     }
     public static class ROMAN{
         static int roman_checker = 0; //если 0 - вывод арабских; 2 - римских; 1 - ошибка.
-        static String [] romans = {"I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX","XXI","XXII","XXIII","XXIV","XXV","XXVI","XXVII","XXVIII","XXIX","XXX","XXXI","XXXII","XXXIII","XXXIV","XXXV","XXXVI","XXXVII","XXXVIII","XXXIX","XL","XLI","XLII","XLIII","XLIV","XLV","XLVI","XLVII","XLVIII","XLIX","L","LI","LII","LIII","LIV","LV","LVI","LVII","LVIII","LIX","LX","LXI","LXII","LXIII","LXIV","LXV","LXVI","LXVII","LXVIII","LXIX","LXX","LXXI","LXXII","LXXIII","LXXIV","LXXV","LXXVI","LXXVII","LXXVIII","LXXIX","LXXX","LXXXI","LXXXII","LXXXIII","LXXXIV","LXXXV","LXXXVI","LXXXVII","LXXXVIII","LXXXIX","XC","XCI","XCII","XCIII","XCIII","XCIV","XCV","XCVI","XCVII","XCVIII","XCIX","C"};
+        static String [] romans = {"I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX","XXI","XXII","XXIII","XXIV","XXV","XXVI","XXVII","XXVIII","XXIX","XXX","XXXI","XXXII","XXXIII","XXXIV","XXXV","XXXVI","XXXVII","XXXVIII","XXXIX","XL","XLI","XLII","XLIII","XLIV","XLV","XLVI","XLVII","XLVIII","XLIX","L","LI","LII","LIII","LIV","LV","LVI","LVII","LVIII","LIX","LX","LXI","LXII","LXIII","LXIV","LXV","LXVI","LXVII","LXVIII","LXIX","LXX","LXXI","LXXII","LXXIII","LXXIV","LXXV","LXXVI","LXXVII","LXXVIII","LXXIX","LXXX","LXXXI","LXXXII","LXXXIII","LXXXIV","LXXXV","LXXXVI","LXXXVII","LXXXVIII","LXXXIX","XC","XCI","XCII","XCIII","XCIV","XCV","XCVI","XCVII","XCVIII","XCIX","C"};
         static String [] arabics = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60","61","62","63","64","65","66","67","68","69","70","71","72","73","74","75","76","77","78","79","80","81","82","83","84","85","86","87","88","89","90","91","92","93","94","95","96","97","98","99","100"};
 
-        public static int ToInt(String number){
+        public static int ToInt(String number){  // из римских в арабские
             
             number = number.toUpperCase();
             int num_int = 0;
@@ -69,7 +66,7 @@ public class calculator {
             }
             return (num_int);
         }
-        public static String ToRoman(int answer){
+        public static String ToRoman(int answer){       // из арабских в римские
             String str_answer = String.valueOf(answer);
             if (roman_checker == 2){
                 for (int i = 0; i < 100; i++){
@@ -84,19 +81,19 @@ public class calculator {
     
     public static class CHECKER{
 
-        public static void limit(int check_number){
+        public static void limit(int check_number){   // выход за пределы
             if (check_number == 0) {
-                System.out.println("chisla ne nahodyatsya v predelah 1-10, libo vvedeni ne po forme x + x.");
+                System.out.println("numbers are not in the range 1-10, or entered not in the form x + x.");
                 System.exit(0);
             } 
         }
-        public static void roman(){
+        public static void roman(){   // числа должны быть либо только римскими, либо только арабскими!
             if (ROMAN.roman_checker == 1){
-                System.out.println("chisla dolzhni bit' tol'ko arbskimi, libo rimskimi.");
+                System.out.println("numbers should only be arabic or roman.");
                 System.exit(0);
             }
         }
-        public static int opearation(int num1_int,int num2_int,String operation){
+        public static int opearation(int num1_int,int num2_int,String operation){   // смотрим какую операцию ввел пользователь
             int answer = 0;
             if (operation.equals("+")){
                 answer = CALCULATION.plus(num1_int,num2_int);
@@ -112,7 +109,7 @@ public class calculator {
 
     }
 
-    public static class CALCULATION{
+    public static class CALCULATION{  // считаем пример
         public static int plus(int num1_int,int num2_int){
             return (num1_int + num2_int);
         }
